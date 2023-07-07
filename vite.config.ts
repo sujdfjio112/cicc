9aa32eb477aca0336b4b4840173c519ec1a6d130
Gavin Xu <pengfeixu@hengtiansoft.com>
2023-07-07 10:30:00 +0800

    change rules

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,25 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
+
+export default defineConfig({
+    base: './',
   plugins: [react()],
-  resolve: {
-    alias: {
-      // 将 svg 文件的路径指定为项目根目录下的 "src" 目录
-      '@/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-})
+    // 配置路径别名
+        resolve: {
+          alias: {
+            "@": path.resolve(__dirname, "src"),
+          },
+        },
+      server: {
+          port: 3000,
+          proxy: {
+            "/api": {
+              target: "https://yourBaseUrl",
+              changeOrigin: true,
+              cookieDomainRewrite: "",
+              secure: false,
+            },
+          },
+        },
+  });
