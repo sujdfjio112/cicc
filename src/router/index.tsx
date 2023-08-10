99e928ea9156e7455b5440c7f8916cc36ae388ab
Gavin Xu <pengfeixu@hengtiansoft.com>
2023-08-10 14:21:19 +0800

    6666

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "../pages/home"
-import About from "../pages/About"
+import About from "../pages/about"
 import Communication from "../pages/communication"
 
 const AppRoutes = () => {
