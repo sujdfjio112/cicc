5fe210860319f86734043b4073d5be52d855c392
Gavin Xu <pengfeixu@hengtiansoft.com>
2023-08-11 15:11:03 +0800

    Revert "6666666666"
    This reverts commit 02d56e421d1addf39dd96fcbb284806b4d864732.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
-import dotenv from "dotenv"
-
-dotenv.config({
-  path: ".env.production",
-})
 
 export default defineConfig({
   base: "./",
