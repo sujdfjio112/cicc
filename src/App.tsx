25e46946108c5d326f76d377850f8d4ce9a037e0
Gavin Xu <pengfeixu@hengtiansoft.com>
2023-08-11 11:01:46 +0800

    123456789

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-// import { BrowserRouter as Router } from "react-router-dom"
-// import Routes from "./router/index"
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes />
-//     </Router>
-//   )
-// }
+import { BrowserRouter as Router } from "react-router-dom"
+import Routes from "./router/index"
 
 const App = () => {
-  return <div>231231</div>
+  return (
+    <Router>
+      <Routes />
+    </Router>
+  )
 }
+
+// const App = () => {
+//   return <div>231231</div>
+// }
 
 export default App
